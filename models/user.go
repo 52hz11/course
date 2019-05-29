52bee3857bdbe1a07bd3655a58c89295bce83677
lhw <935841375@qq.com>
2019-05-29 21:44:11 +0800

    id意义改为学号

--- models/user.go
+++ models/user.go
@@ -10,7 +10,7 @@ import (
 )
 
 type User struct {
-	Id     int    `orm:"column(id);auto"`
+	Id     int    `orm:"column(id)"`
 	Name   string `orm:"column(name);size(40);null"`
 	Token  string `orm:"column(token);size(40);null"`
 	Email  string `orm:"column(email);size(40);null"`
